645e55f22372813d7bb127e3959d58c321e69c44
ricRobo4 <artistr3@proton.me>
2022-10-25 14:34:20 -0700

    added comments

--- Controllers/SayHelloController.cs
+++ Controllers/SayHelloController.cs
@@ -1,3 +1,8 @@
+// Richard Johnson
+// 10-25-22
+// This api will take the user's name and return it when they include their name in the URL
+// Peer Review: 
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace JohnsonRSayHelloEndpoint.Controllers;
@@ -7,10 +12,12 @@ namespace JohnsonRSayHelloEndpoint.Controllers;
 public class SayHelloController : ControllerBase
 {
     [HttpGet]
+    // The route is how the user will get to the right part of the API. They need to include their name in the URL
     [Route("Hello/{name}")]
 
     public string SayHello(string name)
     {
+        // This will take the user's name from the URL and print it along with some other text
         return $"Hello there, {name}. I hope you're having a nice day today";
     }
 }
